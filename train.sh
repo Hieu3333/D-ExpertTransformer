65a3f5e2deb8b64f759f711feaf83546373b1ba3
H <hieu30029081@gmail.com>
2025-03-26 13:06:58 +0700

    a

--- train.sh
+++ train.sh
@@ -25,4 +25,4 @@ python train.py \
   --keyword_vocab_size 571 \
   --image_path "/data/eyenet0420" \
   --ann_path "data" \
-  --project_root "/mnt/c/D-ExpertTransformer"
+  --project_root "/workspace/D-ExpertTransformer"
