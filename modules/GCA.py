38a152105215f6d1d00594e235d64c30c6b9e959
H <hieu30029081@gmail.com>
2025-03-31 14:14:48 +0700

    change to element-wise multiplication

--- modules/GCA.py
+++ modules/GCA.py
@@ -12,7 +12,7 @@ class SpatialContext(nn.Module):
         y = self.conv1d(x) #(B,1,H,W)
         y = F.softmax(y.view(b,1,-1)).transpose(1,2).unsqueeze(-1) #(b,h*w,1,1)
         x = x.view(b,d,-1) #(b,d,h*w)
-        x = torch.matmul(x,y) #(b,d,1,1)
+        out = x * y #(b,d,1,1)
         return x
     
 class ChannelContext(nn.Module):
