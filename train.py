cae6966345bf86cd4eb0c54cb98d35db1812b867
H <hieu30029081@gmail.com>
2025-04-01 15:28:35 +0700

    a

--- train.py
+++ train.py
@@ -144,7 +144,7 @@ for epoch in range(current_epoch-1,num_epochs):
             running_loss = 0.0  # Reset running loss
     
     scheduler.step()  
-    if (epoch+1) % 10 != 0:
+    if (epoch+1) % 50 != 0:
         continue
 
     torch.save({
