4667acbd6d75adc7547aaed6a2dfa8e32dc35cb6
H <hieu30029081@gmail.com>
2025-04-01 23:50:54 +0700

    a

--- train.py
+++ train.py
@@ -171,7 +171,7 @@ for epoch in range(current_epoch-1,num_epochs):
             # Generate captions for the whole batch
             # generated_captions = model.generate(images,beam_width=args.beam_width)  # List of strings, length B
             with torch.cuda.amp.autocast():
-                generated_captions, batch_loss = model.generate_beam(images,gt_keyword_tokens)
+                generated_captions, batch_loss = model.generate_beam(images,gt_keyword_tokens,target_tokens)
             # Decode ground truth captions
             for i, image_id in enumerate(image_ids):
                 groundtruth_caption = gt_clinical_desc[i]
@@ -206,7 +206,7 @@ for epoch in range(current_epoch-1,num_epochs):
             gt_keyword_tokens = gt_keyword_tokens.to(device)
             # generated_captions = model.generate(images,beam_width=args.beam_width)
             with torch.cuda.amp.autocast():
-                generated_captions, _ = model.generate_beam(images,gt_keyword_tokens) 
+                generated_captions, _ = model.generate_beam(images,gt_keyword_tokens,target_tokens) 
 
             for i,image_id in enumerate(image_ids):
                 groundtruth_caption = gt_clinical_desc[i]
