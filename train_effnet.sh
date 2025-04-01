6f8a6232ca4a663b5e34c5932a5f3350739c2959
H <hieu30029081@gmail.com>
2025-04-01 10:20:12 +0700

    a

--- train_effnet.sh
+++ train_effnet.sh
@@ -30,5 +30,5 @@ python train.py \
   --image_path "/data/eyenet0420" \
   --ann_path "data" \
   --project_root "/workspace/D-ExpertTransformer" \
-  --from_pretrained "results/checkpoint_epoch_100.pth"
+  --from_pretrained "results/checkpoint_epoch_50.pth"
 
