c29542410c4cb012179f37c99de74d4cd165955e
H <hieu30029081@gmail.com>
2025-04-01 22:46:28 +0700

    remove gca

--- model/model.py
+++ model/model.py
@@ -163,10 +163,13 @@ class VisualEncoder(nn.Module):
         else:
             self.ve = EfficientNet()
 
-        self.gca = GuidedContextAttention(args)
+        # self.gca = GuidedContextAttention(args)
 
     def forward(self,images):
-        return self.gca(self.ve(images))
+        vf = self.ve(images)
+        B,C,_,_ = vf.size()
+        vf = vf.view(B,C,-1)
+        return vf
 
 
 class LanguageDecoderLayer(nn.Module):
