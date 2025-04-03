ec3f44383eeaec5ca756021a09de48ea75a803c0
H <hieu30029081@gmail.com>
2025-04-04 03:55:23 +0700

    a

--- modules/utils.py
+++ modules/utils.py
@@ -8,7 +8,6 @@ def parser_arg():
     parser.add_argument('--max_length',type=int, required=True)
     parser.add_argument('--dataset',type=str, required=True)
     parser.add_argument('--max_gen',type=int, required=True)
-    parser.add_argument('--decoder',type=str, required=True)
     parser.add_argument('--hidden_size',type=int, required=True)
     parser.add_argument('--vocab_size',type=int, required=True)
     parser.add_argument('--ve_name',type=str, required=True)
