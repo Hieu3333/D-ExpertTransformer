7aa39e615c6ff7e2a35f1d077ba0b3afd46e2a57
H <hieu30029081@gmail.com>
2025-04-04 19:33:19 +0700

    a

--- scripts/train_resnet_eye.sh
+++ scripts/train_resnet_eye.sh
@@ -20,7 +20,7 @@ python train_deepeyenet.py \
   --step_size 10 \
   --max_gen 100 \
   --hidden_size 512 \
-  --fc_size 1024 \
+  --fc_size 2048 \
   --vocab_size 2682 \
   --delta1 1 \
   --delta2 0.01 \
