99fcd8293455c2a1acb7f8fb3d3ea9827eaecb30
H <hieu30029081@gmail.com>
2025-04-13 00:14:27 +0700

    a

--- train_deepeyenet.py
+++ train_deepeyenet.py
@@ -199,11 +199,11 @@ for epoch in range(current_epoch-1,num_epochs):
         with open(val_path, "w") as f:
             json.dump(val_results, f, indent=4)
 
-        check_gts = {i: [gt] for i, gt in enumerate(gts_val)}
-        check_res = {i: [re] for i, re in enumerate(res_val)}
-        print(check_gts)
-        print()
-        print(check_res)
+        # check_gts = {i: [gt] for i, gt in enumerate(gts_val)}
+        # check_res = {i: [re] for i, re in enumerate(res_val)}
+        # print(check_gts)
+        # print()
+        # print(check_res)
 
         # Compute evaluation metrics
         eval_scores = compute_scores({i: [gt] for i, gt in enumerate(gts_val)},
