06fb67f44feb290848c46192f0c4d363f3958a65
H <hieu30029081@gmail.com>
2025-04-13 12:03:49 +0700

    a

--- scripts/train_resnet_iu.sh
+++ scripts/train_resnet_iu.sh
@@ -7,7 +7,7 @@ python train_iu.py \
   --use_gca \
   --lr_ed 4e-4 \
   --lr_ve 1e-4 \
-  --warmup_epochs 10 \
+  --warmup_epochs 30 \
   --weight_decay 1e-5 \
   --channel_reduction 4 \
   --num_layers 3 \
