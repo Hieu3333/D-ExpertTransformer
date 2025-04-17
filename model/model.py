c10bc23bb20e0afe637f089ba34003c4fd239784
H <hieu30029081@gmail.com>
2025-04-18 05:37:11 +0700

    a

--- model/model.py
+++ model/model.py
@@ -151,8 +151,6 @@ class TransfusionEncoder(nn.Module):
             self.attn = MultiHeadedAttention(args,mask=False)
         self.depth = depth
         self.dataset = args.dataset
-        self.use_keywords = args.use_keywords
-        self.use_lt = args.use_learnable_tokens
 
         if depth == 0:
             self.vf_proj = nn.Linear(args.encoder_size, args.hidden_size)
@@ -267,10 +265,8 @@ class ExpertTransformer(nn.Module):
         self.dataset = args.dataset
         self.use_contrastive = args.use_contrastive
         self.num_tokens = 5
-        self.use_keywords = args.use_keywords
-        self.use_lt = args.use_learnable_tokens
 
-        if args.use_learnable_tokens:
+        if args.dataset != "deepeyenet":
             self.learnable_tokens = nn.Parameter(torch.randn(args.batch_size,self.num_tokens,args.hidden_size)) #Use learnable tokens
 
 
@@ -298,10 +294,10 @@ class ExpertTransformer(nn.Module):
 
         visual_features = self.visual_encoder(images)
 
-        if self.use_keywords:
+        if self.dataset == "deepeyenet":
             keyword_emb = self.We(gt_keyword_tokens) #B,keyword_length,hidden_size
             keyword_emb = self.language_encoder(keyword_emb,keyword_emb,keyword_emb)
-        elif self.use_lt:
+        else:
             keyword_emb = self.language_encoder(self.learnable_tokens,self.learnable_tokens,self.learnable_tokens)
 
         
@@ -310,19 +306,19 @@ class ExpertTransformer(nn.Module):
         pos_emb = self.wpe(pos)
         x = self.dropout(tok_emb+pos_emb)
 
-        if self.use_keywords or self.use_lt:
-            for i in range(self.num_layers):
-                if i==0:
-                    encoder_features = self.fuser[i](visual_features,keyword_emb)
-                else:
-                    encoder_features = self.fuser[i](encoder_features,encoder_features)
-                    
-        else:
-            for i in range(self.num_layers):
-                if i == 0:
-                    encoder_features = self.fuser[i](visual_features,visual_features)
-                else:
-                    encoder_features = self.fuser[i](encoder_features,encoder_features)        
+        # if self.dataset == "deepeyenet":
+        for i in range(self.num_layers):
+            if i==0:
+                encoder_features = self.fuser[i](visual_features,keyword_emb)
+            else:
+                encoder_features = self.fuser[i](encoder_features,encoder_features)
+                
+        # else:
+        #     for i in range(self.num_layers):
+        #         if i == 0:
+        #             encoder_features = self.fuser[i](visual_features,visual_features)
+        #         else:
+        #             encoder_features = self.fuser[i](encoder_features,encoder_features)        
         for i in range(self.num_layers):
             x = self.contextual_decoder[i](encoder_features,x)
         
