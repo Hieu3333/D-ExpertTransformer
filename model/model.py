933f3186207ddfa56c6ceee88f69d961f3706f67
H <hieu30029081@gmail.com>
2025-04-21 05:33:02 +0700

    a

--- model/model.py
+++ model/model.py
@@ -486,7 +486,7 @@ class ExpertTransformer(nn.Module):
 
 
         optimizer =torch.optim.AdamW(
-            [{'params': self.visual_encoder.parameters(), 'lr': args.lr_ve},
+            [{'params': ve_params, 'lr': args.lr_ve},
              {'params': ed_params, 'lr': args.lr_ed}],
             weight_decay=args.weight_decay,
             amsgrad=True
