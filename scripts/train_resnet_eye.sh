86924216e8082a6dae127b92f552b1a7431ecd2e
H <hieu30029081@gmail.com>
2025-04-22 08:54:06 +0700

    change tokenizer

--- scripts/train_resnet_eye.sh
+++ scripts/train_resnet_eye.sh
@@ -21,7 +21,7 @@ python train_deepeyenet.py \
   --hidden_size 512 \
   --fc_size 2048 \
   --contrastive_proj_dim 256 \
-  --vocab_size 4250 \
+  --vocab_size 3285 \
   --delta1 1 \
   --delta2 0.3 \
   --topk 3 \
