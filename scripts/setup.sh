d59dd46da5f6af53e8180c61b008959833090b75
H <hieu30029081@gmail.com>
2025-04-24 05:30:06 +0700

    a

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,8 +40,8 @@ wget https://raw.githubusercontent.com/cmu-mtlab/meteor/master/data/paraphrase-e
 
 
 
-scp -P 54169 root@143.55.45.86:/workspace/D-ExpertTransformer/results/resnet-deepeyenet/resnet_deepeyenet.pth "/mnt/c/D-ExpertTransformer/results/deepeyenet"
-scp -P 54169 -r root@143.55.45.86:/workspace/D-ExpertTransformer/logs/resnet-deepeyenet/ "/mnt/c/D-ExpertTransformer/logs"
+scp -P 54169 root@143.55.45.86:/workspace/D-ExpertTransformer/results/effnet-diff/efficientnet_deepeyenet.pth "/mnt/c/D-ExpertTransformer/results/deepeyenet"
+scp -P 54169 -r root@143.55.45.86:/workspace/D-ExpertTransformer/logs/effnet-diff/ "/mnt/c/D-ExpertTransformer/logs"
 
 scp -P 54169 root@143.55.45.86:/workspace/D-ExpertTransformer/results/resnet-diff-roco/roco.pth "/mnt/c/D-ExpertTransformer/results/roco"
 scp -P 54169 -r root@143.55.45.86:/workspace/D-ExpertTransformer/roco/logs/resnet-diff-roco/ "/mnt/c/D-ExpertTransformer/roco/logs"
