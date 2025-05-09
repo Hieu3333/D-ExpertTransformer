d0858122209d3a5005514d669398c29f90f89b8e
H <hieu30029081@gmail.com>
2025-05-10 02:48:52 +0700

    use mixed precision for training

--- train_deepeyenet_da.py
+++ train_deepeyenet_da.py
@@ -132,7 +132,8 @@ for epoch in range(current_epoch-1,num_epochs):
             # print("desc_tokens:",desc_tokens)
             # print("target_tokens:",target_tokens)
             # print('gt:',gt_clinical_desc)
-            outputs, loss = model(images=images,tokens=desc_tokens, gt_keyword_tokens=gt_keyword_tokens, targets=target_tokens)
+            with torch.cuda.amp.autocast():
+                outputs, loss = model(images=images,tokens=desc_tokens, gt_keyword_tokens=gt_keyword_tokens, targets=target_tokens)
             loss = loss / args.accum_steps  # Normalize for gradient accumulation
 
             loss.backward()
