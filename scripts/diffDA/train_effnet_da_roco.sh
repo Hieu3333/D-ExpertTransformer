28755705da89f714ab2a9f9cd5055a435b359e9a
H <hieu30029081@gmail.com>
2025-05-10 18:08:24 +0700

    a

---
+++ scripts/diffDA/train_effnet_da_roco.sh
@@ -0,0 +1,40 @@
+python train_deepeyenet_da.py \
+  --exp_name "effnet-diff" \
+  --epochs 50 \
+  --dataset "roco" \
+  --ve_name "efficientnet" \
+  --use_gca \
+  --use_beam \
+  --lr_ed 2e-4 \
+  --lr_ve 1e-4 \
+  --weight_decay 1e-4 \
+  --channel_reduction 4 \
+  --warmup_epochs 0 \
+  --num_layers 3 \
+  --num_layers_da 1 \
+  --save_path "results" \
+  --batch_size 64 \
+  --accum_steps 1 \
+  --early_stopping 10 \
+  --max_length 100 \
+  --step_size 10 \
+  --max_gen 100 \
+  --hidden_size 512 \
+  --fc_size 2048 \
+  --contrastive_proj_dim 256 \
+  --vocab_size 20000 \
+  --delta1 1 \
+  --delta2 0.3 \
+  --topk 3 \
+  --temperature 1 \
+  --lambda_init 0.8 \
+  --dropout 0.0 \
+  --beam_width 3 \
+  --encoder_size 2048 \
+  --num_heads 8 \
+  --diff_num_heads 4 \
+  --num_workers 16 \
+  --image_path "" \
+  --ann_path "" \
+  --project_root "/workspace/D-ExpertTransformer" \
+
