aa725f620d599b7f244bda8cfd4dab785c42e7ae
H <98133081+Hieu3333@users.noreply.github.com>
2025-05-11 21:30:53 +0700

    a

--- scripts/diffDA/train_effnet_da.sh
+++ scripts/diffDA/train_effnet_da.sh
@@ -36,6 +36,6 @@ python train_deepeyenet.py \
   --image_path "data/eyenet0420" \
   --ann_path "data" \
   --project_root "/workspace/D-ExpertTransformer" \
-  --from_pretrained "results/effnet-diff/efficientnet_deepeyenet.pth"
+
 
 
