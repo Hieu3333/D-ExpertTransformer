0944d13685617d03dd30119b0ae1ea7dbac52072
H <98133081+Hieu3333@users.noreply.github.com>
2025-05-15 15:38:00 +0700

    a

--- scripts/train_effnet_eye.sh
+++ scripts/train_effnet_eye.sh
@@ -1,5 +1,5 @@
 python train_deepeyenet.py \
-  --exp_name "effnet-deepeyenet-2-layers" \
+  --exp_name "effnet-deepeyenet-4-layers" \
   --epochs 50 \
   --dataset "deepeyenet" \
   --ve_name "efficientnet" \
@@ -10,7 +10,7 @@ python train_deepeyenet.py \
   --weight_decay 1e-4 \
   --channel_reduction 4 \
   --warmup_epochs 0 \
-  --num_layers 2 \
+  --num_layers 4 \
   --save_path "results" \
   --batch_size 64 \
   --accum_steps 1 \
